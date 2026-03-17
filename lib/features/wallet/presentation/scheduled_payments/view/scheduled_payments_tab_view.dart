6e5f823c4f13e7df161d4ef93b84d6a9860efecb
VainAmigo <tonyginger15@gmail.com>
2026-03-17 11:25:17 +0600

    fix

---
+++ lib/features/wallet/presentation/scheduled_payments/view/scheduled_payments_tab_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ScheduledPaymentsTabView extends StatefulWidget {
+  const ScheduledPaymentsTabView({super.key});
+
+  @override
+  State<ScheduledPaymentsTabView> createState() =>
+      _ScheduledPaymentsTabViewState();
+}
+
+class _ScheduledPaymentsTabViewState extends State<ScheduledPaymentsTabView> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
